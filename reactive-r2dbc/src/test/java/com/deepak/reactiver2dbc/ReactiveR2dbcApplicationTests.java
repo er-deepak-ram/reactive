257df4c62efa49ebc59b892db402bb34cb9d0ba7
Deepak Kumar Ram <35396750+er-deepak-ram@users.noreply.github.com>
2021-06-30 14:41:47 +0530

    Initial commit for ractive rest api

---
+++ reactive-r2dbc/src/test/java/com/deepak/reactiver2dbc/ReactiveR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.reactiver2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveR2dbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
